5b6e514ef4a09bbf7157f3d3bc5bfa4dbfdebb14
user <volodimirkomarnickij28@gmail.com>
2023-09-28 15:38:39 +0300

    Rewrote revenue/views using Subquery & OuterRef

--- revenue/views.py
+++ revenue/views.py
@@ -1,26 +1,65 @@
-from django.db.models import Sum
-from rest_framework import status
+from django.db.models import Sum, OuterRef, Subquery
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 
+from spend.models import SpendStatistic
 from .models import RevenueStatistic
 from .serializers import RevenueRetrieveSerializer, RevenueSerializer
 
 
 class RevenueStatisticsView(APIView):
     def get(self, request):
-        queryset = RevenueStatistic.objects.values("name", "date").annotate(
+        spend_subquery = SpendStatistic.objects.filter(revenuestatistic=OuterRef('pk')).values('revenuestatistic')
+
+        total_spend_subquery = Subquery(
+            SpendStatistic.objects.filter(revenuestatistic=OuterRef("pk"))
+            .values("revenuestatistic")
+            .annotate(total=Sum("spend"))
+            .values("total")[:1]
+        )
+
+        total_impressions_subquery = Subquery(
+            SpendStatistic.objects.filter(revenuestatistic=OuterRef("pk"))
+            .values("revenuestatistic")
+            .annotate(total=Sum("impressions"))
+            .values("total")[:1]
+        )
+
+        total_clicks_subquery = Subquery(
+            SpendStatistic.objects.filter(revenuestatistic=OuterRef("pk"))
+            .values("revenuestatistic")
+            .annotate(total=Sum("clicks"))
+            .values("total")[:1]
+        )
+
+        total_conversions_subquery = Subquery(
+            SpendStatistic.objects.filter(revenuestatistic=OuterRef("pk"))
+            .values("revenuestatistic")
+            .annotate(total=Sum("conversion"))
+            .values("total")[:1]
+        )
+
+        queryset = RevenueStatistic.objects.annotate(
             total_revenue=Sum("revenue"),
-            total_spend=Sum("spend__spend"),
-            total_impressions=Sum("spend__impressions"),
-            total_clicks=Sum("spend__clicks"),
-            total_conversions=Sum("spend__conversion"),
+            total_spend=total_spend_subquery,
+            total_impressions=total_impressions_subquery,
+            total_clicks=total_clicks_subquery,
+            total_conversions=total_conversions_subquery,
         )
 
         truncated_data = []
         for item in queryset:
-            item["date"] = item["date"].strftime("%Y-%m-%d")
-            truncated_data.append(item)
+            item_dict = {
+                "name": item.name,
+                "date": item.date.strftime("%Y-%m-%d"),
+                "total_revenue": item.total_revenue,
+                "total_spend": item.total_spend,
+                "total_impressions": item.total_impressions,
+                "total_clicks": item.total_clicks,
+                "total_conversions": item.total_conversions,
+            }
+            truncated_data.append(item_dict)
 
         serializer = RevenueRetrieveSerializer(truncated_data, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
